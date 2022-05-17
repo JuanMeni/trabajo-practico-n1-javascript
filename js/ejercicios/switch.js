7fe29af71896852b86b769afb1d041df7e5f1ff4
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-17 10:52:10 -0300

    Se uso de manera correcta el switch

--- js/ejercicios/switch.js
+++ js/ejercicios/switch.js
@@ -1,15 +1,15 @@
 let estacion = prompt("Ingrese su estacion preferida: 1: Verano, 2: Otoño, 3: Invierno, 4: Primavera");
-if( estacion === "1"){
-    document.write("sandwich de miga");
-}else if( estacion === "2"){
-    document.write("locro")
-}else if( estacion === "3"){
-    document.write("guiso")
-}else if( estacion === "4"){
-    document.write("cerveza")
-}else{
-    document.write("Escribio una opcion incorrecta")
-}
+// if( estacion === "1"){
+//     document.write("sandwich de miga");
+// }else if( estacion === "2"){
+//     document.write("locro")
+// }else if( estacion === "3"){
+//     document.write("guiso")
+// }else if( estacion === "4"){
+//     document.write("cerveza")
+// }else{
+//     document.write("Escribio una opcion incorrecta")
+// }
 
 // utilizar varios else if es considerado una mala practica, para eso esta switch
 // switch (opcion){
@@ -25,3 +25,20 @@ if( estacion === "1"){
 //     default: 
 //     todas el codigo a mostrar s no se selecciono ningun caso anterior
 // }
+
+switch(estacion){
+    case "1":
+        document.write("Sandwich de miga")
+        break;
+    case "2":
+        document.write("Locro")
+        break;
+    case "3":
+        document.write("Guiso")
+        break;
+    case "4":
+        document.write("Cerveza")
+        break;
+    default:
+        document.write("Selecciono una opcion que no corresponde")
+}
