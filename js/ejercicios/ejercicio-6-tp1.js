ba617d93e827406c15f96d72067d3a659fd4e673
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-17 10:22:34 -0300

    correccion al ejercicio 6

--- js/ejercicios/ejercicio-6-tp1.js
+++ js/ejercicios/ejercicio-6-tp1.js
@@ -1,9 +1,10 @@
 let numero1 = parseInt(prompt("Escriba un numero"));
 let numero2 = parseInt(prompt("Escriba un segundo numero"));
 
-if(numero1 > numero2){
-    document.write("El numero mayor es: " + numero2);
-}
-else{
-    document.write("El numero mayor es: " + numero2);
+if (numero1 > numero2) {
+  document.write("El numero mayor es: " + numero2);
+} else if (numero1 === numero2) {
+  document.write("el numero 1 es igual al numero 2");
+} else {
+  document.write("El numero mayor es: " + numero2);
 }
