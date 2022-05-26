75a53778e8e5235c279929cdd1b8920d597ce37c
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-26 10:24:36 -0300

    se agrego mas elementos en array

--- js/arrays.js
+++ js/arrays.js
@@ -3,12 +3,38 @@ let usuario = [];
 
 let series = ["Naruto",2 , "GOT",8 ,"Rick & Morty", 4,];
 document.write(series);
+
 // acceder a un elemento del arreglo
+
 document.write("<br>"+ series[0]);
 document.write("<br>"+ series[4]);
 console.log(series.length);
+
 // bucle para mostrar todos los elementos del arreglo
 
+for(let posicion = 0; posicion < series.length; posicion ++){
+    document.write("<br>Elemento: "+ series[posicion]);
+}
+
+// agregamos un elemento al final del arreglo
+series.push("ozark");
+
+for(let posicion = 0; posicion < series.length; posicion ++){
+    document.write("<br>Elemento: "+ series[posicion]);
+}
+
+// agregar un elemento en una posicion en particular.
+series.splice(3,0, "Breaking bad");
+
+document.write("<hr>");
+for(let posicion = 0; posicion < series.length; posicion ++){
+    document.write("<br>Elemento: "+ series[posicion]);
+}
+
+// Eliminar un elemento de una posicion en particular
+series.splice(3,1,);
+
+document.write("<hr>");
 for(let posicion = 0; posicion < series.length; posicion ++){
     document.write("<br>Elemento: "+ series[posicion]);
 }
