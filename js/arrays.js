f6de8def1654f6dbb34fdfde7da185ba2bcc20f0
Juan Menichetti <jpmenichetti13@gmail.com>
2022-05-31 10:36:43 -0300

    se utilizo funciones flechas en el apartado de array

--- js/arrays.js
+++ js/arrays.js
@@ -4,6 +4,14 @@ let usuario = [];
 let series = ["Naruto",2 , "GOT",8 ,"Rick & Morty", 4,];
 document.write(series);
 
+const imprimirArreglos = (titulo) => {
+    document.write("<hr>");
+    document.write(`<h3>${titulo}</h3>`);
+for(let posicion = 0; posicion < series.length; posicion ++){
+    document.write("<br>Elemento: "+ series[posicion]);
+}
+}
+
 // acceder a un elemento del arreglo
 
 document.write("<br>"+ series[0]);
@@ -11,30 +19,33 @@ document.write("<br>"+ series[4]);
 console.log(series.length);
 
 // bucle para mostrar todos los elementos del arreglo
+imprimirArreglos("Arreglo original")
 
-for(let posicion = 0; posicion < series.length; posicion ++){
-    document.write("<br>Elemento: "+ series[posicion]);
-}
+// for(let posicion = 0; posicion < series.length; posicion ++){
+//     document.write("<br>Elemento: "+ series[posicion]);
+// }
 
 // agregamos un elemento al final del arreglo
 series.push("ozark");
 
-for(let posicion = 0; posicion < series.length; posicion ++){
-    document.write("<br>Elemento: "+ series[posicion]);
-}
+// for(let posicion = 0; posicion < series.length; posicion ++){
+//     document.write("<br>Elemento: "+ series[posicion]);
+// }
 
+imprimirArreglos("Se agrego un elemento al final del elemento")
 // agregar un elemento en una posicion en particular.
 series.splice(3,0, "Breaking bad");
 
-document.write("<hr>");
-for(let posicion = 0; posicion < series.length; posicion ++){
-    document.write("<br>Elemento: "+ series[posicion]);
-}
-
+// document.write("<hr>");
+// for(let posicion = 0; posicion < series.length; posicion ++){
+//     document.write("<br>Elemento: "+ series[posicion]);
+// }
+imprimirArreglos("Se agrego un elemento en una posicion particular.")
 // Eliminar un elemento de una posicion en particular
 series.splice(3,1,);
 
-document.write("<hr>");
-for(let posicion = 0; posicion < series.length; posicion ++){
-    document.write("<br>Elemento: "+ series[posicion]);
-}
+// document.write("<hr>");
+// for(let posicion = 0; posicion < series.length; posicion ++){
+//     document.write("<br>Elemento: "+ series[posicion]);
+// }
+imprimirArreglos("Se elimino un elemento de una posicion en particular.")
